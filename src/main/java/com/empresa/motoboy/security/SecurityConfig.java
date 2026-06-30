998d8bcb125a7d0de74427e7439e26764d84ceab
pauloviola16 <pcviola.dev@gmail.com>
2026-06-30 15:19:08 -0300

    feat: adicionado jwt filter e sevice

--- src/main/java/com/empresa/motoboy/security/SecurityConfig.java
+++ src/main/java/com/empresa/motoboy/security/SecurityConfig.java
@@ -1,15 +1,22 @@
 package com.empresa.motoboy.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -19,21 +26,32 @@ public class SecurityConfig {
 
                 .csrf(csrf -> csrf.disable())
 
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(
+                                "/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/usuarios"
+                                "/v3/api-docs/**"
                         )
                         .permitAll()
 
+                        .requestMatchers("/usuarios/**")
+                        .hasRole("ADMIN")
+
                         .anyRequest()
                         .authenticated()
-
                 )
 
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
 
                 .build();
     }
