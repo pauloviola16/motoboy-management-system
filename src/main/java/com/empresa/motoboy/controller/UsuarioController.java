676bcf4f630abc901b8e37775e7b013e7d662803
pauloviola16 <pcviola.dev@gmail.com>
2026-06-30 13:53:41 -0300

    feat: adicionando o userController

--- src/main/java/com/empresa/motoboy/controller/UsuarioController.java
+++ src/main/java/com/empresa/motoboy/controller/UsuarioController.java
@@ -1,12 +1,13 @@
 package com.empresa.motoboy.controller;
 
 import com.empresa.motoboy.dto.UsuarioRequestDTO;
-import com.empresa.motoboy.dto.UsuarioResponseDTO;
+import com.empresa.motoboy.model.Usuario;
 import com.empresa.motoboy.service.UsuarioService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -16,13 +17,40 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public UsuarioResponseDTO criar(
-            @RequestBody @Valid
-            UsuarioRequestDTO dto
-    ) {
+    public Usuario cadastrar(@RequestBody UsuarioRequestDTO dto) {
+        return usuarioService.cadastrar(dto);
+    }
 
-        return usuarioService.criar(dto);
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<Usuario> listarTodos() {
+        return usuarioService.listarTodos();
+    }
 
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Usuario buscarPorId(@PathVariable Long id) {
+        return usuarioService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Usuario atualizar(
+            @PathVariable Long id,
+            @RequestBody UsuarioRequestDTO dto) {
+
+        return usuarioService.atualizar(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deletar(@PathVariable Long id) {
+        usuarioService.deletar(id);
+    }
+
+    @PutMapping("/{id}/promover-admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Usuario promoverParaAdmin(@PathVariable Long id) {
+        return usuarioService.promoverParaAdmin(id);
     }
 }
