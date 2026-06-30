f2eda54176cc07c53802769ae90f69a7f7d468bf
pauloviola16 <pcviola.dev@gmail.com>
2026-06-30 16:20:33 -0300

    feat: adiconando tela de login e seu controller

---
+++ src/main/java/com/empresa/motoboy/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.empresa.motoboy.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+    public class LoginController {
+
+        @GetMapping("/login")
+        public String login() {
+            return "login";
+        }
+    }
+
--- src/main/java/com/empresa/motoboy/security/SecurityConfig.java
+++ src/main/java/com/empresa/motoboy/security/SecurityConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -24,15 +22,17 @@ public class SecurityConfig {
 
                 .csrf(csrf -> csrf.disable())
 
-
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(
-                                "/auth/**",
+                                "/login",
+                                "/usuarios/cadastro",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**"
-                        )
-                        .permitAll()
+                        ).permitAll()
 
                         .requestMatchers("/usuarios/**")
                         .hasRole("ADMIN")
@@ -41,7 +41,33 @@ public class SecurityConfig {
                         .authenticated()
                 )
 
+                .formLogin(form -> form
+
+                        .loginPage("/login")
+
+                        .loginProcessingUrl("/login")
+
+                        .defaultSuccessUrl("/escalas", true)
+
+                        .failureUrl("/login?error=true")
+
+                        .permitAll()
+                )
+
+                .logout(logout -> logout
+
+                        .logoutUrl("/logout")
+
+                        .logoutSuccessUrl("/login?logout=true")
+
+                        .permitAll()
+                )
 
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
